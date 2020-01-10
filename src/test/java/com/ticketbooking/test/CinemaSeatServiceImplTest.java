1293ea9d1ab223ce05188c888d2a55d9e57669fe
himanshucis <59689580+himanshucis@users.noreply.github.com>
2020-01-10 17:33:48 +0530

    Update CinemaSeatServiceImplTest.java

--- src/test/java/com/ticketbooking/test/CinemaSeatServiceImplTest.java
+++ src/test/java/com/ticketbooking/test/CinemaSeatServiceImplTest.java
@@ -150,29 +150,4 @@ class CinemaSeatServiceImplTest {
 		assertEquals(updateSeat.getId(),seat.getId());
 	}
 	
-	public Query getQuery() {
-		Query query = entityManager.createNativeQuery("INSERT INTO seats (id,seat_no,status,user_id) VALUES" + "\n" + 
-				                         "(1,1,0,null),\n" +
-				                         "(2,2,0,null),\n" + 
-				                         "(3,3,0,null),\n" + 
-				                         "(4,4,0,null),\n" + 
-				                         "(5,5,0,null),\n" + 
-				                         "(6,6,0,null),\n" + 
-				                         "(7,7,0,null),\n" + 
-				                         "(8,8,0,null),\n" + 
-				                         "(9,9,0,null),\n" + 
-				                         "(10,10,0,null),\n" + 
-				                         "(11,11,0,null),\n" + 
-				                         "(12,12,0,null),\n" + 
-				                         "(13,13,0,null),\n" + 
-				                         "(14,14,0,null),\n" + 
-				                         "(15,15,0,null),\n" + 
-				                         "(16,16,0,null),\n" + 
-				                         "(17,17,0,null),\n" + 
-				                         "(18,18,0,null),\n" + 
-				                         "(19,19,0,null),\n" + 
-				                         "(20,20,0,null);\n");
-		return query;	
-	}
-	
 }
